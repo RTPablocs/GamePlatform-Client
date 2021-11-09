6353764b5261e8cf4cb6faedeac22d59cf75eebf
AdriRameti <adri7agu@gmail.com>
2021-11-09 20:11:42 +0100

    update ranking

--- servidor/models/ranking.js
+++ servidor/models/ranking.js
@@ -4,10 +4,10 @@ const typeScore = mongoose.Schema({
     id:{type:String},
     nameGame:{type:String},
     datePlay:{type:String},
-    score:{type:String}
+    score:{type:Number}
 })
 const singleScore = mongoose.Schema({
-    user:{type: String},
+    user:{type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     game:[typeScore]
 })
 const RankingSchema = mongoose.Schema({
--- servidor/routes/api/ranking.js
+++ servidor/routes/api/ranking.js
@@ -1,5 +1,7 @@
+var mongoose = require('mongoose');
 var router = require('express').Router();
 const Ranking = require('../../models/ranking');
+var auth = require('../auth');
 
 router.get('/general',async(req,res)=>{
     try{
@@ -45,5 +47,51 @@ router.post('/', function(req, res, next){
     rank.save().then(function(){
         return res.json(rank);
     });
-  });
+});
+router.post('/update',function(req,res){
+    var id = req.payload.id;
+    var idObject =  mongoose.Types.ObjectId(id);
+    var date = new Date();
+    var game = {
+        "nameGame": req.body.nameGame,
+        "datePlay": date,
+        "score": req.body.score
+    }
+    var newUser = {
+        "singleScore":{
+            "user":id,
+            "game":[
+                game
+            ]
+        }
+    }
+
+    try{
+        Ranking.aggregate()
+        .unwind({path:"$singleScore"})
+        .match({"singleScore.user":idObject})
+        .then(function(data){
+            if(data.length == 0){
+                let rank;
+                rank = new Ranking(newUser);
+                rank.save().then(function(){
+                    return res.json(rank);
+                })
+            }else{
+                var idRank = mongoose.Types.ObjectId(data[0]._id);
+                Ranking.findById(idRank).then(function(rank){
+                    var games = rank.singleScore[0].game
+                    console.log(rank.singleScore[0].game);
+                    games.push(game);
+                    rank.save().then(function(data){
+                        return res.json(data);
+                    })
+                })
+            }
+        })
+
+    }catch(e){
+        throw e;
+    }
+})
   module.exports = router;
